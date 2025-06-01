58ae42fd22dafbc21140687fa1480797b4b50112
javierlovera <loverajavier0@gmail.com>
2025-06-01 09:53:42 -0300

    agrugue las imagenes y el carrusel

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -9,5 +9,28 @@
   <body>
     <h1>mi primer pagina web</h1>
     <script src="assets/js/bootstrap.bundle.min.js" ></script>
+    <!---carrusel-->
+                <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets\img\coati.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets\img\mariposas.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets\img\lagartija.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  </div>
+  <!--fin de carrusel-->
   </body>
 </html>
